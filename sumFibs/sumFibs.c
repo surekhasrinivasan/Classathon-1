0c9171ec0608a5a4d1c09c72846f9e31eb12f402
Surekha Srinivasan <surekhasrao@gmail.com>
2018-04-17 16:12:15 +0000

    sumFibs algorithm solved

---
+++ sumFibs/sumFibs.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cs50.h>
+
+//declare the function
+int sumFibs(int num);
+
+//define the function
+int sumFibs(int num)
+{
+    int result = 0;
+    int startNum = 0;
+    int nextNum = 1;
+
+    while (startNum <= num)
+    {
+        if (startNum % 2 != 0)
+        {
+            result = result + startNum;
+        }
+
+        int sum = startNum + nextNum;
+        startNum = nextNum;
+        nextNum = sum;
+    }
+    return result;
+}
+
+int main(int argc, string argv[])
+{
+    //check if the user do not enter number
+    if (argc != 2)
+    {
+        printf("Usage: ./factorialize sumFibs\n");
+        return 1;
+    }
+
+    int num = atoi(argv[1]);
+
+    //output the result
+    printf("%i\n", sumFibs(num));
+    return 1;
+}
